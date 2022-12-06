e442382d1eba2d342d4ef7bc2f56ac267a833045
Tessa van der Heiden <tessavdheiden@gmail.com>
2022-12-06 10:35:32 +0100

    unit test for main

--- main.py
+++ main.py
@@ -10,6 +10,12 @@ class Item(BaseModel):
 app = FastAPI()
 
 
+# Should be placed on top so "items" does not become a category
+@app.get("/items/{item_id}")
+async def get_items(item_id: int, count: int = 1):
+    return {"fetch": f"Fetched {count} of {item_id}"}
+
+
 @app.post("/{category}")
 async def create_item(category: str, amount: int, item: Item):
     return {"category": category, "amount": amount, "item": item}
---
+++ test_main.py
@@ -0,0 +1,23 @@
+from fastapi.testclient import TestClient
+
+# Import our app from main.py.
+from main import app
+
+# Instantiate the testing client with our app.
+client = TestClient(app)
+
+
+# Test with and without the query parameter
+def test_api_get_item_query_parameter():
+    r = client.get("/items/42")
+    assert r.status_code == 200
+    assert r.json() == {"fetch": "Fetched 1 of 42"}
+    r = client.get("/items/42?count=10")
+    assert r.status_code == 200
+    assert r.json() == {"fetch": "Fetched 10 of 42"}
+
+
+# Test a malformed URL
+def test_api_malformed_url():
+    r = client.get("/items")
+    assert r.status_code != 200
